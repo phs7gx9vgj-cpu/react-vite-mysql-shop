8bce7b2349a9d980b295ad932a4d0793e8222025
tran hoang giang <tranhgiang97@gmail.com>
2025-12-29 12:40:13 +0700

    Add missing CSS files for components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom"
-import "./Navbar.css" // Import file CSS mới
+import "./Navbar.css"
 
 export default function Navbar() {
   const location = useLocation();
